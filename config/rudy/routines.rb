2ce7dded38510c5ebaa8df8d1277e42ccd36fcc4
delano <delano@solutious.com>
2009-04-28 02:07:22 -0400

    Up to date DSL syntax

--- config/rudy/routines.rb
+++ config/rudy/routines.rb
@@ -13,7 +13,7 @@ routines do
         disks do
           # Rudy creates an EBS volume for each instance, attaches
           # it, gives it a filesystem, and mounts it. 
-          #create "/rudy/disk1"
+          create "/rudy/disk1"
         end
       end
       
@@ -21,16 +21,19 @@ routines do
       # configuration for release. 
       #release stage.app.startup
       release do
-        #before :root do
+        before :root do
           #rm :f, :r, '/mnt/delanotes'
-          #df :h
-          #touch 'home-poop'
+          df :h
+          touch 'home-poop'
           #upload "/Users/delano/.ssh/git-delano_rsa", "~/.ssh"
-        #end
+        end
         git :delano do
           privatekey '/Users/delano/.ssh/id_rsa'
           remote :heroku
-          path "/mnt/delanotes"
+          path "/rudy/disk1/app/delanotes"
+        end
+        after :delano do
+          touch 'home-poop'
         end
       end
       
@@ -39,7 +42,7 @@ routines do
         #before :root => '/a/nonexistent/script'
         disks do
           # Rudy unmounts the EBS volume and deletes it. Careful! 
-          #destroy "/rudy/disk1"
+          destroy "/rudy/disk1"
         end
       end
       
