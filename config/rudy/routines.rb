99fe4f5062029deb49b8342d5f45badf81bf8fe6
delano <delano@solutious.com>
2009-05-04 12:45:48 -0400

    Example installs rye and rudy

--- config/rudy/routines.rb
+++ config/rudy/routines.rb
@@ -19,7 +19,7 @@ routines do
   installdeps do
     script :root do
       gem_install "test-spec", "rspec", "camping", "fcgi", "memcache-client"
-      gem_install "mongrel"
+      gem_install "mongrel", "rye", "rudy"
       gem_install 'ruby-openid', :v, "2.0.4"   # thin requires 2.0.x
       gem_install "rack", :v, "0.9.1"
       gem_install "macournoyer-thin"           # need 1.1.0 which works with rack 0.9.1
